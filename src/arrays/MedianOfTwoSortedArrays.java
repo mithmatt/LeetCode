04c935ad347f70f8a6b03568a2998883762ea067
mithmatt <mithunma@andrew.cmu.edu>
2015-07-29 22:12:10 -0700

    ...

---
+++ src/arrays/MedianOfTwoSortedArrays.java
@@ -0,0 +1,67 @@
+package arrays;
+
+/**
+ * 
+ * https://leetcode.com/problems/median-of-two-sorted-arrays/
+ * 
+ * @author Matt
+ *
+ */
+public class MedianOfTwoSortedArrays {
+
+	public static void main(String[] args) {
+		int[] nums1 = { 1, 3, 5, 7, 9 };
+		int[] nums2 = { 2, 4, 6, 8, 10 };
+
+		System.out.println(findMedianSortedArrays(nums1, nums2));
+	}
+
+	private static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+		int m = nums1.length;
+		int n = nums2.length;
+		int[] result = new int[m + n];
+
+		if (result.length == 0)
+			return -1;
+
+		int i = 0, j = 0, k = 0;
+		int mid = result.length / 2;
+
+		boolean even = false;
+
+		if (result.length % 2 == 0)
+			even = true;
+
+		while (i < m && j < n) {
+			if (nums1[i] < nums2[j])
+				result[k++] = nums1[i++];
+			else
+				result[k++] = nums2[j++];
+
+			if (even && k == mid + 1)
+				return ((double) (result[k - 1] + result[k - 2]) / 2);
+			else if (!even && k == mid + 1)
+				return result[k - 1];
+		}
+
+		while (i < m) {
+			result[k++] = nums1[i++];
+
+			if (even && k == mid + 1)
+				return ((double) (result[k - 1] + result[k - 2]) / 2);
+			else if (!even && k == mid + 1)
+				return result[k - 1];
+		}
+
+		while (j < n) {
+			result[k++] = nums2[j++];
+
+			if (even && k == mid + 1)
+				return ((double) (result[k - 1] + result[k - 2]) / 2);
+			else if (!even && k == mid + 1)
+				return result[k - 1];
+		}
+
+		return -1;
+	}
+}
---
+++ src/numbers/RomanToInteger.java
@@ -0,0 +1,55 @@
+package numbers;
+
+/**
+ * 
+ * https://leetcode.com/problems/roman-to-integer/
+ * 
+ * @author Matt
+ *
+ */
+public class RomanToInteger {
+
+	public static void main(String[] args) {
+		System.out.println(romanToInt("LMIX"));
+	}
+
+	private static int romanToInt(String s) {
+
+		int curr = 0, prev = 0, result = 0;
+
+		for (int i = s.length() - 1; i >= 0; i--) {
+			curr = getInt(s.charAt(i));
+
+			if (curr >= prev)
+				result += curr;
+			else
+				result -= curr;
+
+			prev = curr;
+		}
+
+		return result;
+	}
+
+	private static int getInt(char roman) {
+
+		switch (roman) {
+		case 'I':
+			return 1;
+		case 'V':
+			return 5;
+		case 'X':
+			return 10;
+		case 'L':
+			return 50;
+		case 'C':
+			return 100;
+		case 'D':
+			return 500;
+		case 'M':
+			return 1000;
+		default:
+			return 0;
+		}
+	}
+}
