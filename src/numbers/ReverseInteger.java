a2d874c113e293db8cc9365d6de17a918577bc38
mithmatt <mithunma@andrew.cmu.edu>
2015-08-07 03:03:09 -0700

    .

---
+++ src/numbers/ReverseInteger.java
@@ -0,0 +1,29 @@
+package numbers;
+
+/**
+ * 
+ * https://leetcode.com/problems/reverse-integer/
+ * 
+ * @author Matt
+ *
+ */
+public class ReverseInteger {
+
+	public static void main(String[] args) {
+		System.out.println(reverse(-217386127));
+	}
+
+	private static int reverse(int x) {
+		int rev = 0;
+
+		while (x != 0) {
+			if (Math.abs(rev) > 214748364)
+				return 0;
+
+			rev = rev * 10 + x % 10;
+			x /= 10;
+		}
+
+		return rev;
+	}
+}
